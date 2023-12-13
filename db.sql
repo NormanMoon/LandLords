81f2f1bd96c8a3d585dab05201774d92e9692037
NoroMoon <normannmoon@gmail.com>
2023-12-13 09:53:49 -0500

    added more postgress commands to test with

--- db.sql
+++ db.sql
@@ -1,7 +1,7 @@
 
 
 CREATE TABLE restraunts (
-    id BIGSERIAL NOT NULL, 
+    id BIGSERIAL NOT NULL PRIMARY KEY, 
     name VARCHAR(50) NOT NULL, 
     location VARCHAR(50) NOT NULL, 
     price_range INT NOT NULL CHECK(price_range >= 1 and price_range <= 5)
@@ -9,3 +9,4 @@ CREATE TABLE restraunts (
 
 INSERT INTO restraunts (id, name, location, price_range) values (123, 'McDs', 'New York', 3); 
 INSERT INTO restraunts (id, name, location, price_range) values (124, 'Pizza Hut', 'Vegas', 2); 
+INSERT INTO restraunts (name, location, price_range) values('wendys', 'miami', 7); 
